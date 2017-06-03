79a53bcf5ec7e18d8458efbe26de81e7d12208c5
Зотов Денис <zot-dv@yandex.ru>
2017-06-03 19:57:44 +0300

    Custom dereferers & shorteners (hardcoded)

--- src/url-backstage.js
+++ src/url-backstage.js
@@ -2,6 +2,20 @@
 
 console.log("URL-BACKSTAGE INJECTED");
 
+// Can get hidden link directly from arguments
+// Purpose: hiding original HTTP referer header
+const dereferers = [
+    /^https?:\/\/vk\.com\/away\.php.*?[?&]to=([a-z]+%[^&]+)/
+];
+
+// Can get hidden link only after visiting short link or via special services
+// Purpose: making short link from long one, collecting statistics
+const shorteners = [
+    /^https?:\/\/vk\.cc\/[0-9A-Za-z_-]+/,
+    /^https?:\/\/goo\.gl\/[0-9A-Za-z_-]+/,
+    /^https?:\/\/tinyurl\.com\/[0-9A-Za-z_-]+/
+];
+
 const initCache = () => new Promise(onCacheReady => {
     const dbrq = indexedDB.open('UrlBackstage', 1);
     dbrq.onupgradeneeded = event => {
@@ -15,7 +29,8 @@ const initCache = () => new Promise(onCacheReady => {
     };
 });
 
-const cache_getOrLoad = (db, url, loader) => new Promise((resolve, reject) => {
+
+const cache_getOrLoad = (db, url, loader) => new Promise(resolve => {
     let url_noprotocol = url.match(/^[a-z]+:\/\/(.+)/)[1];
     let tr = db.transaction('Links', 'readonly');
     let store = tr.objectStore('Links');
@@ -24,14 +39,14 @@ const cache_getOrLoad = (db, url, loader) => new Promise((resolve, reject) => {
         let result = event.target.result;
         if (result) {
             resolve(result.longUrl);
-        } else {
-            loader(url).then(r => {
-                let tr = db.transaction('Links', 'readwrite');
-                let store = tr.objectStore('Links');
-                store.add({ shortUrl:url_noprotocol, longUrl:r });
-                resolve(r);
-            });
+            return;
         }
+        loader(url).then(r => {
+            let tr = db.transaction('Links', 'readwrite');
+            let store = tr.objectStore('Links');
+            store.add({ shortUrl:url_noprotocol, longUrl:r });
+            resolve(r);
+        });
     };
 });
 
@@ -42,73 +57,75 @@ const decodeURIWithCharset = uri => {
         ? parseInt(c.substr(1), 16) 
         : c.charCodeAt(0)));
     return decoder.decode(data);
-}
+};
 
 const decodeHTMLEntities = str => {
     const decoder = document.createElement('div');
     decoder.innerHTML = str;
     return decoder.innerText;
-}
+};
 
-const initLinksWatcher = (db) => {
-    const url_pattern = /^https?:\/\/[\w.-]+\w+\//;
+const initLinksWatcher = db => {
+    const url_pattern = /^[0-9a-z]+?:\/\//;
 
-    const vkcc_checkLink = url => new Promise((resolve, reject) => {
-        if (typeof url !== 'string') throw new TypeError('URL should be a string');
-        if (!url_pattern.test(url)) throw new TypeError('Invalid URL: '+ url);
+    const vkcc_checkLink = href => new Promise(resolve => {
+        if (typeof href !== 'string') throw new TypeError('URL should be a string');
+        if (!url_pattern.test(href)) throw new TypeError('Invalid URL: '+ href);
         const vk_api = 'https://api.vk.com/method/utils.checkLink?url=';
         let xhr = new XMLHttpRequest();
-        xhr.open('GET', vk_api + encodeURIComponent(url));
+        xhr.open('GET', vk_api + encodeURIComponent(href));
         xhr.onreadystatechange = () => {
-            if (xhr.readyState === xhr.DONE) {
-                if (xhr.status === 200) {
-                    let r = JSON.parse(xhr.responseText);
-                    if (r.response) {
-                        if (r.response.status === 'processing') {
-                            setTimeout(() => { vkcc_checkLink(url); }, 2000);
-                        } else {
-                            r.response.link = decodeHTMLEntities(r.response.link);
-                            resolve(r.response);
-                        }
-                    } else if (r.error) {
-                        throw 'VK API error ' + r.error.error_code + 
-                            ': ' + r.error.error_msg;
-                    } else { 
-                        throw 'VK API bad response ' + xhr.responseText;
-                    }
-                } else {
-                    throw 'VK API response code ' + xhr.status;
-                }
+            if (xhr.readyState !== xhr.DONE) return;
+            if (xhr.status !== 200) throw 'VK API response code ' + xhr.status;
+            let r = JSON.parse(xhr.responseText);
+            if (r.error) throw 'VK API error ' + r.error.error_code +
+                ': ' + r.error.error_msg;
+            if (!r.response) throw 'VK API bad response ' + xhr.responseText;
+            if (r.response.status === 'processing') {
+                setTimeout(() => {
+                    cache_getOrLoad(db, href, vkcc_checkLink).then(resolve);
+                    }, 2500);
+            } else {
+                r.response.link = decodeHTMLEntities(r.response.link);
+                resolve(r.response);
             }
-        }
+        };
         xhr.send();
     });
 
-    const findLinks = el => {
-        let links = el.querySelectorAll('a[href^="/away.php"]');
-        for (let a of links) {
-            let to_enc = a.href.match(/[?&]to=([^&]+)/)[1];
-            let search = decodeURIWithCharset(to_enc);
-            if (/:\/\/vk\.cc\//.test(search)) {
-                cache_getOrLoad(db, search, vkcc_checkLink).then(r => {
-                    a.title = r.link;
-                    // console.log(r);
-                });
-            } else {
-                a.title = search;
-            }
+    const inspectHref = href => new Promise(resolve => {
+        if (!href) return;
+        let changed = false;
+        for (let d of dereferers) {
+            let m = href.match(d);
+            if (!m) continue;
+            href = url_pattern.test(m[1]) ? m[1] : decodeURIWithCharset(m[1]);
+            changed = true;
+            break;
         }
-    }
+        for (let s of shorteners) {
+            if (!s.test(href)) continue;
+            cache_getOrLoad(db, href, vkcc_checkLink).then(resolve);
+            return;
+        }
+        if (changed) resolve({link: href});
+    });
+
+    const findLinks = el => {
+        let links = el.querySelectorAll('a[href]');
+        for (let a of links) inspectHref(a.href).then(r => {
+            a.title = r.link;
+            // console.log(r);
+        });
+    };
 
     const mutobs = new MutationObserver(mutations => {
-        for (let m of mutations) {
-            findLinks(m.target);
-        }
+        for (let m of mutations) findLinks(m.target);
     });
 
     findLinks(document.body);
     mutobs.observe(document.body, {subtree: true, childList: true});
     console.log("URL-BACKSTAGE OK");
-}
+};
 
 initCache().then(initLinksWatcher);
